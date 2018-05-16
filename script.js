acb4c3ed746797e546e0937a8b8a6a04336cf8c1
kcbobbe <kcbobbe@gmail.com>
2018-05-15 22:14:26 -0400

    Finished formatting and added a search feature that fills out the profile of the username that is entered. I did not add any validation for the form.

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 import request from 'superagent'
 
-function getData(){
-  request.get(`https://api.github.com/users/kcbobbe`)
-  .then(function(result) {
-  const url = (result.body.url)
-  const name = ('Name', result.body.name)
-  const bio = ('Bio', result.body.bio)
-  const myPic = ('myPic', result.body.avatar_url)
-  const website = ('webpage',result.body.blog)
-  const company = ('company', result.body.company)
-  // const 
-  console.log(url, name, bio)
-  document.getElementById('myName').innerHTML = name
-  document.getElementById('bio').innerHTML = `<div class = bio>${bio}`
-  document.getElementById('links').innerHTML = 
-  `<ul>
-      <li>Name: ${name}</li>
-      <li>My LinkedIn profile: ${website}</li>
-      <li>GitHub: ${url}</li>
-      <li>Company: ${company}</li>
-  </ul>`
-  document.getElementById('myPic').innerHTML = `<img src = ${myPic}>`
-  
-  // document.getElementById('myName').innerHTML = result;
-})
+function onSubmit (event) {
+  event.preventDefault()
+  console.log('submitted!')
+  const query = document.getElementById('query').value
+  getData(query)
 }
 
-// console.log(getData('url'))
-getData()
+function getData (username) {
+  request.get(`https://api.github.com/users/${username}`)
+    .then(function (result) {
+      const url = (result.body.url)
+      const name = (result.body.name)
+      const bio = (result.body.bio)
+      const myPic = (result.body.avatar_url)
+      const website = (result.body.blog)
+      const company = (result.body.company)
+      document.getElementById('myName').innerHTML = name
+      document.getElementById('bio').innerHTML = `<div class = bio>${bio}`
+      document.getElementById('links').innerHTML = 
+      `<ul>
+          <li><strong>Name</strong>: ${name}</li>
+          <li><strong>My LinkedIn profile: </strong>${website}</li>
+          <li><strong>GitHub: </strong>${url}</li>
+          <li><strong>Company: </strong>${company}</li>
+      </ul>`
+      document.getElementById('myPic').innerHTML = `<img src = ${myPic}>`
+    })
+}
+
+// getData('kcbobbe')
+
+document.getElementById('search-form').addEventListener('submit', onSubmit)
